b3034252c5dfcf62b971e37de492d2b6ba48cd09
MisterC1ean <robert.keller44@icloud.com>
2020-06-08 17:37:34 -0400

    Form is posting to back end for new Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,5 +4,16 @@ class ProjectsController < ApplicationController
     projects = Project.all
     render :json => projects
   end
+
+  def create
+    project = Project.create!(project_params)
+    render :json => project
+  end
   
+  
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :user_id,  images: [])
+  end
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,4 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :user_id
+  attributes :id, :title, :user_id, :images
   has_many :steps
 end
